dc250b837410d06beb5a081fd78a3de42e78a406
k6zma <87338187+k6zma@users.noreply.github.com>
2025-02-04 17:18:19 +0300

    feat: made ping_time type INTERVAL (NUMERIC -> INTERVAL)

--- backend/migrations/000001_container_status_table.up.sql
+++ backend/migrations/000001_container_status_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE container_status (
     id BIGSERIAL PRIMARY KEY,
     ip_address INET NOT NULL UNIQUE,
-    ping_time NUMERIC,
+    ping_time INTERVAL,
     last_successful_ping TIMESTAMP,
     updated_at TIMESTAMP DEFAULT now(),
     created_at TIMESTAMP DEFAULT now()
