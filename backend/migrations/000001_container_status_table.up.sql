16ea3189dc84c5b049a7a12c2e980396b7208a52
k6zma <87338187+k6zma@users.noreply.github.com>
2025-02-04 23:41:53 +0300

    feat: update type of ping time for better mapping

--- backend/migrations/000001_container_status_table.up.sql
+++ backend/migrations/000001_container_status_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE container_status (
     id BIGSERIAL PRIMARY KEY,
     ip_address INET NOT NULL UNIQUE,
-    ping_time INTERVAL,
+    ping_time DOUBLE PRECISION NOT NULL,
     last_successful_ping TIMESTAMP,
     updated_at TIMESTAMP DEFAULT now(),
     created_at TIMESTAMP DEFAULT now()
