59c2776d472801f4d3fbf59807f6df12b3934818
k6zma <87338187+k6zma@users.noreply.github.com>
2025-02-05 11:00:40 +0300

    feat: added new type of flags

--- backend/internal/infrastructure/flags/flags.go
+++ backend/internal/infrastructure/flags/flags.go
@@ -8,18 +8,24 @@ import (
 )
 
 type AppFlags struct {
-	ConfigFilePath string `validate:"required"`
+	ConfigFilePath string `validate:"required,file"`
+	MigrationsPath string `validate:"required,dir"`
+	MigrationsType string `validate:"oneof=apply drop rollback"`
 	LoggerLevel    string `validate:"oneof=debug info warn error dpanic panic fatal"`
 }
 
 func ParseFlags() (*AppFlags, error) {
-	configFile := flag.String("config", "config.json", "Path to json configuration file")
+	configFile := flag.String("config_path", "config.json", "Path to json configuration file")
+	migrationsPath := flag.String("migrations_path", "migrations", "Path to migrations directory")
+	migrationsType := flag.String("migrations_type", "file", "Type of migrations (apply, drop, rollback)")
 	loggerLevel := flag.String("logger_level", "debug", "Logger level (debug, info, warn, error, dpanic, panic, fatal)")
 
 	flag.Parse()
 
 	appFlags := &AppFlags{
 		ConfigFilePath: *configFile,
+		MigrationsPath: *migrationsPath,
+		MigrationsType: *migrationsType,
 		LoggerLevel:    *loggerLevel,
 	}
 
